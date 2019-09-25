4ebaf44e19d3f7572bcfe75e1106df0116613482
Denis Isidoro <denis_isidoro@live.com>
2019-09-25 08:15:04 -0300

    Fix typo

--- src/version.sh
+++ src/version.sh
@@ -1,6 +1,6 @@
 version::code() {
    cd "$SCRIPT_DIR"
-   local -r git_info=$(gsit log -n 1 --pretty=format:'%h%n%ad%n%s' --date=format:'%Y-%m-%d %Hh%M')
+   local -r git_info=$(git log -n 1 --pretty=format:'%h%n%ad%n%s' --date=format:'%Y-%m-%d %Hh%M')
    if [ -z "$git_info" ]; then
       return 1
    else
