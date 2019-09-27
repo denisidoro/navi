b9084c043927c0ec68f30f9bd4e941ed1a5c6361
Denis Isidoro <denisidoro@users.noreply.github.com>
2019-09-27 11:42:49 -0300

    Add suppot for multiline snippets (#91)
    Fix #88

--- src/cheat.sh
+++ src/cheat.sh
@@ -6,10 +6,25 @@ cheat::find() {
    done
 }
 
+cheat::_join_multiline_using_sed() {
+   tr '\n' '\f' \
+      | sed -E 's/\\\f *//g' \
+      | tr '\f' '\n'
+}
+
+cheat::_join_multiline() {
+   if ${NAVI_USE_PERL:-false}; then
+      perl -0pe 's/\\\n *//g' \
+         || cheat::_join_multiline_using_sed
+   else
+      cheat::_join_multiline_using_sed
+   fi
+}
+
 cheat::read_all() {
    for cheat in $(cheat::find); do
       echo
-      cat "$cheat"
+      cat "$cheat" | cheat::_join_multiline
       echo
    done
 }
@@ -18,6 +33,11 @@ cheat::memoized_read_all() {
    if [ -n "${NAVI_CACHE:-}" ]; then
       echo "$NAVI_CACHE"
       return
+   fi
+   if command_exists perl; then
+      export NAVI_USE_PERL=true
+   else
+      export NAVI_USE_PERL=false
    fi
    local -r cheats="$(cheat::read_all)"
    echo "$cheats"
