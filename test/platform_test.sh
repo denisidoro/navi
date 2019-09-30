110ec72ab7e28cae2ba0a164f65a8db2fe4e0d8a
Denis Isidoro <denis_isidoro@live.com>
2019-09-30 11:26:12 -0300

    Remove bad tests

--- test/platform_test.sh
+++ test/platform_test.sh
@@ -5,18 +5,5 @@ existing() {
     | test::equals awk
 }
 
-exit_code() {
-  "$@"
-  echo $?
-}
-
-exists() {
-   platform::command_exists awk \
-    && exit_code \
-    || exit_code \
-    | assert 
-}
-
 test::set_suite "platform"
 test::run "existing_command" existing
-test::run "command_exists" exists
