879fac1bc968c72e81a0d51b91b34ba6a6c34a4c
Denis Isidoro <denis_isidoro@live.com>
2019-11-06 11:46:29 -0300

    lint

--- src/str.sh
+++ src/str.sh
@@ -17,10 +17,10 @@ str::sub() {
 
 str::column() {
    local -r n="${1:-}"
-   local -r separator="${2:-  +}"
+   local -r separator="$(echo "${2:-}" | or "  +")"
 
    if [ -n "$n" ]; then
-      awk -F "$separator" --"{print \$$n}"
+      awk -F "$separator" "{print \$$n}"
    else
       cat
    fi
