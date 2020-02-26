ca20f1549e99ad67199cd6c7dbac5284aa406256
Denis Isidoro <denis_isidoro@live.com>
2020-02-26 12:22:10 -0300

    Add clap definition

--- src/health.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-health::fzf() {
-   if ! command_exists fzf && ! [ $NAVI_ENV -eq "test" ]; then
-      echoerr "You need to install fzf before using navi"
-      echoerr "Please refer to https://github.com/junegunn/fzf for install instructions"
-      exit 66
-   fi
-}
--- test/cheat_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-assert_docker_cheat() {
-   cheat::find | test::contains "docker.cheat"
-}
-
-test::set_suite "cheat"
-test::run "We can find at least one known cheatsheet" assert_docker_cheat
