41c4c7ba7bd314a51a9c4ab0f4bd08dfd72e7fe8
Denis Isidoro <denis_isidoro@live.com>
2020-02-26 18:38:22 -0300

    wip

--- src/core.rs
+++ src/core.rs
@@ -2,38 +2,64 @@ use clap::ArgMatches;
 use regex::Regex;
 use std::error::Error;
 use std::io::Write;
-use std::process::Command;
+use std::process::{Command, Stdio};
+use std::collections::HashMap;
 
 use crate::fzf;
 use crate::parse;
 
 pub fn main(_matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
-    let output = fzf::call(|stdin| {
-        parse::read_all(stdin);
+    let (output, variables) = fzf::call(|stdin| {
+        parse::read_all(stdin)
     });
+
+    // println!("{}, {:#?}", variables.len(), variables);
 
     if output.status.success() {
         let raw_output = String::from_utf8(output.stdout)?;
-        let snippet = raw_output.split('\t').nth(5).unwrap();
+        let mut parts = raw_output.split('\t');
+        parts.next();
+        parts.next();
+        parts.next();
+        let tags = parts.next().unwrap();
+        let comment = parts.next().unwrap();
+        let snippet = parts.next().unwrap();
         let mut full_snippet = String::from(snippet);
-
+        
         let re = Regex::new(r"<(.*?)>").unwrap();
         for cap in re.captures_iter(snippet) {
-            println!("{}", &cap[0]);
+            println!("tags: {}; comment: {}; snippet: {}; cap: {}", tags, comment, snippet, &cap[0]);
             let bracketed_varname = &cap[0];
             let varname = &bracketed_varname[1..bracketed_varname.len() - 1];
+            let k = format!("{};{}", tags, varname);
 
-            let output = fzf::call(|stdin| {
-                stdin.write_all(b"foo\n").unwrap();
-                stdin.write_all(b"bar\n").unwrap();
-                stdin.write_all(b"baz\n").unwrap();
-                stdin
-                    .write_all(format!("{}\n", varname).as_bytes())
+            let suggestions = match variables.get(&k[..]) {
+                Some(c) => {
+                    println!("c: {}", c);
+
+                   let child = Command::new("bash")
+                   .stdout(Stdio::piped())
+                    .arg("-c")
+                    .arg(c)
+                    .spawn()
                     .unwrap();
+                    
+                    String::from_utf8(child.wait_with_output().unwrap().stdout).unwrap()
+                },
+                None => String::from("TODO\n"),
+            };
+
+            let (output, _) = fzf::call(|stdin| {
+                stdin.write_all(b"before\n").unwrap();
+                stdin.write_all(suggestions.as_bytes()).unwrap();
+                stdin.write_all(b"after\n").unwrap();
+                HashMap::new()
             });
 
             let value = String::from_utf8(output.stdout).unwrap();
             full_snippet = full_snippet.replace(bracketed_varname, &value[..value.len() - 1]);
+
+            println!("suggestions: {}; snippet: {}", suggestions, full_snippet);
         }
 
         Command::new("bash")
--- src/parse.rs
+++ src/parse.rs
@@ -3,6 +3,8 @@ use ansi_term::Colour;
 use std::env;
 use std::fs;
 use std::io::Write;
+use std::collections::HashMap;
+use regex::Regex;
 
 fn gen_snippet(snippet: &str, line: &str) -> String {
     if snippet.is_empty() {
@@ -20,7 +22,15 @@ fn limit_str(text: &str, length: usize) -> String {
     }
 }
 
-fn read_file(path: &str, stdin: &mut std::process::ChildStdin) {
+fn parse_variable_line(line: &str) -> (&str, &str, &str) {
+    let re = Regex::new(r"^\$\s*([^:]+):(.*)").unwrap();
+    let caps = re.captures(line).unwrap();
+    let variable = caps.get(1).unwrap().as_str().trim();
+    let command = caps.get(2).unwrap().as_str();
+    (variable, command, command)
+}
+
+fn read_file(path: &str, variables: &mut HashMap<String, String>, stdin: &mut std::process::ChildStdin) {
     let mut tags = String::from("");
     let mut comment = String::from("");
     let mut snippet = String::from("");
@@ -29,10 +39,12 @@ fn read_file(path: &str, stdin: &mut std::process::ChildStdin) {
         for l in lines {
             let line = l.unwrap();
             if line.starts_with('%') {
-                tags = line;
+                tags = String::from(&line[2..]);
             } else if line.starts_with('#') {
-                comment = line;
+                comment = String::from(&line[2..]);
             } else if line.starts_with('$') {
+                let (variable, command, _) = parse_variable_line(&line[..]);
+                variables.insert(format!("{};{}", tags, variable), String::from(command));
             }
             // TODO
             else if line.ends_with('\\') {
@@ -47,11 +59,11 @@ fn read_file(path: &str, stdin: &mut std::process::ChildStdin) {
                 match stdin.write(
                     format!(
                         "{col0}\t{col1}\t{col2}\t{tags}\t{comment}\t{snippet}\t\n",
-                        col0 = Colour::Red.paint(limit_str(&tags[2..], 16)),
-                        col1 = Colour::Blue.paint(limit_str(&comment[2..], 26)),
+                        col0 = Colour::Red.paint(limit_str(&tags[..], 16)),
+                        col1 = Colour::Blue.paint(limit_str(&comment[..], 26)),
                         col2 = &full_snippet,
-                        tags = &tags[2..],
-                        comment = &comment[2..],
+                        tags = tags,
+                        comment = comment,
                         snippet = &full_snippet
                     )
                     .as_bytes(),
@@ -64,17 +76,23 @@ fn read_file(path: &str, stdin: &mut std::process::ChildStdin) {
     }
 }
 
-pub fn read_all(stdin: &mut std::process::ChildStdin) {
+pub fn read_all(stdin: &mut std::process::ChildStdin) -> HashMap<String, String> {
+    let mut variables: HashMap<String, String> = HashMap::new();
+
     let folders_str =
         env::var("NAVI_PATH").unwrap_or(format!("{}/../../cheats", filesystem::exe_path_string()));
     let folders = folders_str.split(':');
+
     for folder in folders {
         let paths = fs::read_dir(folder).unwrap();
         for path in paths {
             read_file(
                 path.unwrap().path().into_os_string().to_str().unwrap(),
+                &mut variables,
                 stdin,
             );
         }
     }
+
+    variables
 }
