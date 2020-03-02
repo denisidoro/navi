6852d2e100e545e98eab5f3f9c6d7c0d84f384da
Denis Isidoro <denis_isidoro@live.com>
2020-03-02 13:01:44 -0300

    wip

--- src/cheat.rs
+++ src/cheat.rs
@@ -1,11 +1,19 @@
+use crate::display;
 use crate::filesystem;
 use crate::option::Config;
 
-use ansi_term::Colour;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
+
+pub struct SuggestionOpts {
+    pub header_lines: u8,
+    pub column: Option<u8>,
+    pub multi: bool,
+}
+
+pub type Value = (String, Option<SuggestionOpts>);
 
 fn gen_snippet(snippet: &str, line: &str) -> String {
     if snippet.is_empty() {
@@ -15,64 +23,94 @@ fn gen_snippet(snippet: &str, line: &str) -> String {
     }
 }
 
-fn limit_str(text: &str, length: usize) -> String {
-    if text.len() > length {
-        format!("{}…", &text[..length - 1])
-    } else {
-        format!("{:width$}", text, width = length)
+fn parse_opts(text: &str) -> SuggestionOpts {
+    let mut header_lines: u8 = 0;
+    let mut column: Option<u8> = None;
+    let mut multi = false;
+
+    let mut parts = text.split(' ');
+
+    while let Some(p) = parts.next() {
+        match p {
+            "--multi" => multi = true,
+            "--header" => header_lines = parts.next().unwrap().parse::<u8>().unwrap(),
+            "--column" => column = Some(parts.next().unwrap().parse::<u8>().unwrap()),
+            _ => (),
+        }
+    }
+
+    SuggestionOpts {
+        header_lines: header_lines,
+        column: column,
+        multi: multi,
     }
 }
 
-fn parse_variable_line(line: &str) -> (&str, &str, Option<&str>) {
+fn parse_variable_line(line: &str) -> (&str, &str, Option<SuggestionOpts>) {
     let re = Regex::new(r"^\$\s*([^:]+):(.*)").unwrap();
     let caps = re.captures(line).unwrap();
     let variable = caps.get(1).unwrap().as_str().trim();
     let mut command_plus_opts = caps.get(2).unwrap().as_str().split("---");
     let command = command_plus_opts.next().unwrap();
-    let opts = command_plus_opts.next();
+    let opts = match command_plus_opts.next() {
+        Some(o) => Some(parse_opts(o)),
+        None => None,
+    };
     (variable, command, opts)
 }
 
 fn read_file(
     path: &str,
-    variables: &mut HashMap<String, String>,
+    variables: &mut HashMap<String, Value>,
     stdin: &mut std::process::ChildStdin,
 ) {
-    // println!("reading {}", path);
     let mut tags = String::from("");
     let mut comment = String::from("");
     let mut snippet = String::from("");
 
+    let (tag_width, comment_width) = display::widths();
+
     if let Ok(lines) = filesystem::read_lines(path) {
         for l in lines {
             let line = l.unwrap();
+
+            // tag
             if line.starts_with('%') {
                 tags = String::from(&line[2..]);
-            } else if line.starts_with('#') {
-                comment = String::from(&line[2..]);
-            } else if line.starts_with('$') {
-                let (variable, command, _) = parse_variable_line(&line[..]);
-                variables.insert(format!("{};{}", tags, variable), String::from(command));
             }
-            // TODO
+            // comment
+            else if line.starts_with('#') {
+                comment = String::from(&line[2..]);
+            }
+            // variable
+            else if line.starts_with('$') {
+                let (variable, command, opts) = parse_variable_line(&line[..]);
+                variables.insert(
+                    format!("{};{}", tags, variable),
+                    (String::from(command), opts),
+                );
+            }
+            // snippet with line break
             else if line.ends_with('\\') {
                 snippet = if !snippet.is_empty() {
                     format!("{}{}", &snippet[..snippet.len() - 2], line)
                 } else {
                     line
                 }
-            } else if line.is_empty() {
-            } else {
+            }
+            // blank
+            else if line.is_empty() {
+            }
+            // snippet
+            else {
                 let full_snippet = gen_snippet(&snippet, &line);
                 match stdin.write(
-                    format!(
-                        "{col0}\t{col1}\t{col2}\t{tags}\t{comment}\t{snippet}\t\n",
-                        col0 = Colour::Red.paint(limit_str(&tags[..], 16)),
-                        col1 = Colour::Blue.paint(limit_str(&comment[..], 26)),
-                        col2 = &full_snippet,
-                        tags = tags,
-                        comment = comment,
-                        snippet = &full_snippet
+                    display::format_line(
+                        &tags[..],
+                        &comment[..],
+                        &full_snippet[..],
+                        tag_width,
+                        comment_width,
                     )
                     .as_bytes(),
                 ) {
@@ -84,8 +122,8 @@ fn read_file(
     }
 }
 
-pub fn read_all(config: &Config, stdin: &mut std::process::ChildStdin) -> HashMap<String, String> {
-    let mut variables: HashMap<String, String> = HashMap::new();
+pub fn read_all(config: &Config, stdin: &mut std::process::ChildStdin) -> HashMap<String, Value> {
+    let mut variables: HashMap<String, Value> = HashMap::new();
 
     let fallback = format!("{}/cheats", filesystem::exe_path_string());
     let folders_str = config.path.as_ref().unwrap_or(&fallback);
