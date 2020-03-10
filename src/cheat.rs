c7e9f6b759df2547833b34893051f077393bc7c0
Denis Isidoro <denis_isidoro@live.com>
2020-03-10 13:59:13 -0300

    Defensive cheats folder

--- src/cheat.rs
+++ src/cheat.rs
@@ -130,7 +130,8 @@ fn read_file(
 pub fn read_all(config: &Config, stdin: &mut std::process::ChildStdin) -> HashMap<String, Value> {
     let mut variables: HashMap<String, Value> = HashMap::new();
 
-    let fallback = format!("{}/cheats", filesystem::exe_path_string());
+    let current_exe = filesystem::exe_path_string();
+    let fallback = format!("{}/cheats:{}/../cheats:{}/../libexec/cheats", current_exe, current_exe, current_exe);
     let folders_str = config.path.as_ref().unwrap_or(&fallback);
     let folders = folders_str.split(':');
 
