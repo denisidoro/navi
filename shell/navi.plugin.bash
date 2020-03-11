2ac30999c2af0bc9607871f13f2f8b37ad9450ab
Denis Isidoro <denis_isidoro@live.com>
2020-03-11 11:31:04 -0300

    Bump

--- shell/navi.plugin.bash
+++ shell/navi.plugin.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 __call_navi() {
-    navi --print
+    printf "$(navi --print)"
 }
 
 bind '"\C-g": " \C-b\C-k \C-u`__call_navi`\e\C-e\C-a\C-y\C-h\C-e\e \C-y\ey\C-x\C-x\C-f"'
--- shell/navi.plugin.zsh
+++ shell/navi.plugin.zsh
@@ -2,7 +2,7 @@
 
 _call_navi() {
    local -r buff="$BUFFER"
-   local -r r="$(navi --print)"
+   local -r r="$(printf "$(navi --print </dev/tty)")"
    zle kill-whole-line
    zle -U "${buff}${r}"
 }
