226a3f26eb4e3303861061a0bcd4b367a882cd75
Denis Isidoro <denis_isidoro@live.com>
2020-03-12 21:01:00 -0300

    Add support for navi fn

--- src/cmds/func.rs
+++ src/cmds/func.rs
@@ -1,7 +1,18 @@
 use std::error::Error;
+use std::process::Command;
 
-use super::aux;
-
-pub fn main(_func: String, _args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    aux::abort("calling `navi fn`", 201)
+pub fn main(func: String, args: Vec<String>) -> Result<(), Box<dyn Error>> {
+        match func.as_str() {
+            "url::open" => {
+                let url = args.into_iter().nth(0).unwrap();
+                let cmd = format!("url=\"{}\"; (xdg-open \"$url\" 2> /dev/null || open \"$url\" 2> /dev/null) &disown", url);
+            Command::new("bash")
+            .arg("-c")
+            .arg(cmd.as_str())
+            .spawn()
+            .unwrap();
+            Ok(())
+            },
+            _ => unreachable!("")
+        }
 }
