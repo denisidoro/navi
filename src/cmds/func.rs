e5067e9a8e8feb4fbcc21b2cdbf0ca0df79367e3
Denis Isidoro <denis_isidoro@live.com>
2020-03-12 21:01:25 -0300

    linting

--- src/cmds/func.rs
+++ src/cmds/func.rs
@@ -2,17 +2,17 @@ use std::error::Error;
 use std::process::Command;
 
 pub fn main(func: String, args: Vec<String>) -> Result<(), Box<dyn Error>> {
-        match func.as_str() {
-            "url::open" => {
-                let url = args.into_iter().nth(0).unwrap();
-                let cmd = format!("url=\"{}\"; (xdg-open \"$url\" 2> /dev/null || open \"$url\" 2> /dev/null) &disown", url);
+    match func.as_str() {
+        "url::open" => {
+            let url = args.into_iter().next().unwrap();
+            let cmd = format!("url=\"{}\"; (xdg-open \"$url\" 2> /dev/null || open \"$url\" 2> /dev/null) &disown", url);
             Command::new("bash")
-            .arg("-c")
-            .arg(cmd.as_str())
-            .spawn()
-            .unwrap();
+                .arg("-c")
+                .arg(cmd.as_str())
+                .spawn()
+                .unwrap();
             Ok(())
-            },
-            _ => unreachable!("")
         }
+        _ => unreachable!(""),
+    }
 }
