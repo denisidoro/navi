e40a0e2dc487c2305201b74243ad93137386ab9f
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-03-13 08:27:19 -0300

    Reuse variables (#252)
    Fixes #251

--- src/cmds/core.rs
+++ src/cmds/core.rs
@@ -147,17 +147,20 @@ fn replace_variables_from_snippet(
     for cap in re.captures_iter(snippet) {
         let bracketed_varname = &cap[0];
         let varname = &bracketed_varname[1..bracketed_varname.len() - 1];
-        let k = format!("{};{}", tags, varname);
 
-        let value = match variables.get(&k[..]) {
-            Some(suggestion) => prompt_with_suggestions(&config, suggestion, &values),
-            None => prompt_without_suggestions(varname),
-        };
+        if values.get(varname).is_none() {
+            let k = format!("{};{}", tags, varname);
 
-        values.insert(varname.to_string(), value.clone());
+            let value = match variables.get(&k[..]) {
+                Some(suggestion) => prompt_with_suggestions(&config, suggestion, &values),
+                None => prompt_without_suggestions(varname),
+            };
 
-        interpolated_snippet =
-            interpolated_snippet.replace(bracketed_varname, gen_replacement(&value[..]).as_str());
+            values.insert(varname.to_string(), value.clone());
+
+            interpolated_snippet = interpolated_snippet
+                .replace(bracketed_varname, gen_replacement(&value[..]).as_str());
+        }
     }
 
     interpolated_snippet
