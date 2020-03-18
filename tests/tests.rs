0a092ef7e7337bc64660f17a2a18ec41e31112dc
Denis Isidoro <denis_isidoro@live.com>
2020-03-18 20:34:44 -0300

    linting

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,7 +3,7 @@ mod tests {
     #[test]
     fn it_works() {
         //let _x = navi::handle_config(navi::config_from_iter(
-            //"navi best trivial".split(' ').collect(),
+        //"navi best trivial".split(' ').collect(),
         //));
         // assert_eq!(x, 3);
     }
