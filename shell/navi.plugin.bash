ed0759f9e321233bd3a58dfb3f77071dd2d890d0
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-03-24 08:38:42 -0300

    Fix multiline behavior in the bash widget (#308)
    Fixes #306 
    
    This only happens when using the bash widget

--- shell/navi.plugin.bash
+++ shell/navi.plugin.bash
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
 __call_navi() {
-    printf "$(navi --print)"
+    local -r result="$(navi --print)"
+    local -r linecount="$(echo "$result" | wc -l)"
+
+    if [[ "$linecount" -lt 2 ]]; then
+        printf "$result"
+        return 0
+    fi
+
+    IFS=$'\n'
+    local i=1;
+    for line in $result; do
+        if echo "$line" | grep -q '\\$'; then
+            printf "${line::-1} "
+        elif [[ "$i" -eq "$linecount" ]]; then
+            printf "$line "
+        else 
+            printf "${line}; "
+        fi
+        i=$((i+1))
+    done
 }
 
 bind '"\C-g": " \C-b\C-k \C-u`__call_navi`\e\C-e\C-a\C-y\C-h\C-e\e \C-y\ey\C-x\C-x\C-f"'
