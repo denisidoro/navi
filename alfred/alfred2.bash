fdfdc3dc190d66e4e0164577e168a7e94fbe4d8a
Denis Isidoro <denis.isidoro@uber.com>
2020-04-25 15:20:11 -0300

    wip

--- alfred/alfred2.bash
+++ alfred/alfred2.bash
@@ -2,16 +2,8 @@
 
 source "${HOME}/.bashrc"
 
-_interpolate() {
-        local -r snippet="$1"
-        local -r varname="$2"
-        local -r value="${!varname}"
-
-        echo "$snippet" | sed "s/<${varname}>/${value}/g"
-}
-
 if [ -n "${varname:-}" ]; then 
-        echo -n "$(navi interpolate "$snippet" "$varname")"
+        echo -n "$(navi transform)"
 else
         echo -n "$snippet"
 fi
