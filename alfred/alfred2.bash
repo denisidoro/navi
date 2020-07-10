bf940c0c7cf3a453156249113dd4025b765badab
Denis Isidoro <denis.isidoro@uber.com>
2020-07-10 10:05:30 -0300

    Remove alfred hacks

--- alfred/alfred2.bash
+++ alfred/alfred2.bash
@@ -4,7 +4,7 @@ source "${HOME}/.bashrc"
 export PATH="/usr/local/bin:$PATH"
 
 if [ -n "${varname:-}" ]; then
-   echo -n "$(navi alfred transform)" | sed 's/   Needs Review       Add new hash function to predicate-eval//' | tr -d '\n'
+   echo -n "$(navi alfred transform)" | tr -d '\n'
 else
    echo -n "$snippet"
 fi
