7de5426fc78b422e28799f6709a7d7a69e05a0fd
Denis Isidoro <denis.isidoro@uber.com>
2020-08-14 12:01:33 -0300

    Aug-14 12h01: core.rs, Cargo.toml, Cargo.lock

--- src/flows/core.rs
+++ src/flows/core.rs
@@ -152,7 +152,12 @@ fn replace_variables_from_snippet(
                 .ok_or_else(|| anyhow!("No suggestions"))
                 .and_then(|suggestion| {
                     let mut new_suggestion = suggestion.clone();
-                    new_suggestion.0 = replace_variables_from_snippet(&new_suggestion.0, tags, variables.clone(), config)?;
+                    new_suggestion.0 = replace_variables_from_snippet(
+                        &new_suggestion.0,
+                        tags,
+                        variables.clone(),
+                        config,
+                    )?;
 
                     prompt_with_suggestions(
                         variable_name,
