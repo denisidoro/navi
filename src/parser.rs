e2d24f68c41107a3034d2f6538ce860f4fb3ae2f
Denis Isidoro <denis.isidoro@uber.com>
2020-08-25 10:49:07 -0300

    Aug-25 10h49: parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -14,7 +14,53 @@ use std::io::Write;
 lazy_static! {
     pub static ref VAR_LINE_REGEX: Regex =
         Regex::new(r"^\$\s*([^:]+):(.*)").expect("Invalid regex");
+    pub static ref VAR_TLDR_REGEX: Regex =
+        Regex::new(r"\{\{(.*?)\}\}").expect("Invalid regex");
+    pub static ref NON_VAR_CHARS_REGEX: Regex =
+        Regex::new(r"[^\da-zA-Z_]").expect("Invalid regex");
 }
+
+static markdown: &'static str = r#"# tar
+
+> Archiving utility.
+> Often combined with a compression method, such as gzip or bzip.
+> More information: <https://www.gnu.org/software/tar>.
+
+- Create an archive from files:
+
+`tar cf {{target.tar}} {{file1}} {{file2}} {{file3}}`
+
+- Create a gzipped archive:
+
+`tar czf {{target.tar.gz}} {{file1}} {{file2}} {{file3}}`
+
+- Create a gzipped archive from a directory using relative paths:
+
+`tar czf {{target.tar.gz}} -C {{path/to/directory}} .`
+
+- Extract a (compressed) archive into the current directory:
+
+`tar xf {{source.tar[.gz|.bz2|.xz]}}`
+
+- Extract a (compressed) archive into the target directory:
+
+`tar xf {{source.tar[.gz|.bz2|.xz]}} -C {{directory}}`
+
+- Create a compressed archive, using archive suffix to determine the compression program:
+
+`tar caf {{target.tar.xz}} {{file1}} {{file1}} {{file2}} {{file3}}`
+
+- List the contents of a tar file:
+
+`tar tvf {{source.tar}}`
+
+- Extract files matching a pattern:
+
+`tar xf {{source.tar}} --wildcards {{"*.html"}}`
+
+- Extract a specific file without preserving the folder structure:
+
+`tar xf {{source.tar}} {{source.tar/path/to/extract}} --strip-components={{depth_to_strip}}`"#;
 
 fn parse_opts(text: &str) -> Result<FinderOpts, Error> {
     let mut multi = false;
@@ -138,8 +184,9 @@ fn write_cmd(
     }
 }
 
-fn read_file(
-    path: &str,
+fn read_lines(
+    lines: impl Iterator<Item = Result<String, Error>>,
+    id: &str,
     variables: &mut VariableMap,
     visited_lines: &mut HashSet<u64>,
     writer: &mut dyn Writer,
@@ -150,9 +197,9 @@ fn read_file(
     let mut snippet = String::from("");
     let mut should_break = false;
 
-    for (line_nr, line_result) in filesystem::read_lines(path)?.enumerate() {
+    for (line_nr, line_result) in lines.enumerate() {
         let line = line_result
-            .with_context(|| format!("Failed to read line nr.{} from `{}`", line_nr, path))?;
+            .with_context(|| format!("Failed to read line nr.{} from `{}`", line_nr, id))?;
 
         if should_break {
             break;
@@ -209,7 +256,7 @@ fn read_file(
                 format!(
                     "Failed to parse variable line. See line number {} in cheatsheet `{}`",
                     line_nr + 1,
-                    path
+                    id
                 )
             })?;
             variables.insert_suggestion(&tags, &variable, (String::from(command), opts));
@@ -236,6 +283,55 @@ fn read_file(
     Ok(())
 }
 
+fn read_file(
+    path: &str,
+    variables: &mut VariableMap,
+    visited_lines: &mut HashSet<u64>,
+    writer: &mut dyn Writer,
+    stdin: &mut std::process::ChildStdin,
+) -> Result<(), Error> {
+    let lines = filesystem::read_lines(path)?;
+    read_lines(lines, path, variables, visited_lines, writer, stdin)
+}
+
+fn convert_tldr_vars(line: &str) -> String {
+    let caps = VAR_TLDR_REGEX.find_iter(&line);
+    let mut new_line: String = line.to_string();
+    for cap in caps {
+        let braced_var = cap.as_str();
+        let var = &braced_var[2..braced_var.len()-2];
+        let new_var = NON_VAR_CHARS_REGEX.replace_all(var, "_");
+        let bracketed_var = format!("<{}>", new_var);
+        new_line = new_line.replace(braced_var, &bracketed_var);
+    }
+    new_line
+}
+
+fn convert_tldr(line: &str) -> Result<String, Error> {
+    let new_line = if line.starts_with('-') {
+        format!("{}{}", "# ", &line[2..line.len()-1])
+    } else if line.starts_with('`') {
+        String::from(convert_tldr_vars(&line[1..line.len()-1]))
+    } else if line.starts_with('%') {
+        line.to_string()
+    } else {
+        "".to_string()
+    };
+    Ok(new_line)
+}
+
+fn read_tldr_markdown(
+    variables: &mut VariableMap,
+    visited_lines: &mut HashSet<u64>,
+    writer: &mut dyn Writer,
+    stdin: &mut std::process::ChildStdin,
+) -> Result<(), Error> {
+    let prefix = r#"% markdown, test
+    "#.lines().map(|line| convert_tldr(line));
+    let lines = markdown.lines().map(|line| convert_tldr(line.trim()));
+    read_lines(prefix.chain(lines), "markdown", variables, visited_lines, writer, stdin)
+}
+
 fn paths_from_path_param<'a>(env_var: &'a str) -> impl Iterator<Item = &'a str> + 'a {
     env_var.split(':').filter(|folder| folder != &"")
 }
@@ -249,6 +345,10 @@ pub fn read_all(
     let mut found_something = false;
     let mut visited_lines = HashSet::new();
     let paths = filesystem::cheat_paths(config);
+
+    // TODO: remove
+    read_tldr_markdown(&mut variables, &mut visited_lines, writer, stdin);
+    return Ok(variables);
 
     if paths.is_err() {
         welcome::cheatsheet(writer, stdin);
