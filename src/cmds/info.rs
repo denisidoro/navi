9bd61adc88a3a51e2bcc0ed42062e062add38470
Denis Isidoro <denis.isidoro@uber.com>
2020-09-15 15:27:42 -0300

    Sep-15 15h27: info.rs

--- src/cmds/info.rs
+++ src/cmds/info.rs
@@ -1,4 +1,6 @@
 use anyhow::Error;
+use crate::filesystem::default_cheat_pathbuf;
+use crate::common::filesystem::pathbuf_to_string;
 
 #[derive(Debug)]
 pub enum Info {
@@ -7,7 +9,7 @@ pub enum Info {
 
 pub fn main(info: &Info) -> Result<(), Error> {
     match info {
-        Info::CheatsPath => println!("foo"),
+        Info::CheatsPath => println!("{}", pathbuf_to_string(default_cheat_pathbuf()?)?),
     }
     Ok(())
 }
