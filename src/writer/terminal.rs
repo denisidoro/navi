243a2c8472068cad0bdb4d97787b94ebe5db4ceb
Denis Isidoro <denisidoro@users.noreply.github.com>
2021-04-06 10:33:05 -0300

    Use crossterm instead of termion and terminal_width

--- src/writer/terminal.rs
+++ src/writer/terminal.rs
@@ -1,7 +1,8 @@
 use crate::env_vars;
 use crate::finder;
 use crate::structures::item::Item;
-use crate::terminal::{self, color};
+use crate::terminal;
+use crate::terminal::style::{style, Color};
 use crate::writer;
 use std::cmp::max;
 use std::collections::HashSet;
@@ -18,26 +19,30 @@ pub fn parse_env_var<T: FromStr>(varname: &str) -> Option<T> {
     }
 }
 
+fn parse_ansi(varname: &str, default: Color) -> Color {
+    let value: Option<String> = parse_env_var(varname);
+    if let Some(v) = value {
+        if let Some(a) = terminal::parse_ansi(&v) {
+            return a;
+        }
+    }
+    default
+}
+
 lazy_static! {
-    pub static ref TAG_COLOR: color::AnsiValue =
-        color::AnsiValue(parse_env_var(env_vars::TAG_COLOR).unwrap_or(14));
-    pub static ref COMMENT_COLOR: color::AnsiValue =
-        color::AnsiValue(parse_env_var(env_vars::COMMENT_COLOR).unwrap_or(4));
-    pub static ref SNIPPET_COLOR: color::AnsiValue =
-        color::AnsiValue(parse_env_var(env_vars::SNIPPET_COLOR).unwrap_or(7));
+    pub static ref TAG_COLOR: Color = parse_ansi(env_vars::TAG_COLOR, Color::Cyan);
+    pub static ref COMMENT_COLOR: Color = parse_ansi(env_vars::COMMENT_COLOR, Color::Blue);
+    pub static ref SNIPPET_COLOR: Color = parse_ansi(env_vars::SNIPPET_COLOR, Color::White);
     pub static ref TAG_WIDTH_PERCENTAGE: u16 = parse_env_var(env_vars::TAG_WIDTH).unwrap_or(20);
     pub static ref COMMENT_WIDTH_PERCENTAGE: u16 = parse_env_var(env_vars::COMMENT_WIDTH).unwrap_or(40);
 }
 
 pub fn preview(comment: &str, tags: &str, snippet: &str) {
     println!(
-        "{comment_color}{comment} {tag_color}{tags} \n{snippet_color}{snippet}",
-        comment = comment.to_string(),
-        tags = format!("[{}]", tags),
-        snippet = writer::fix_newlines(snippet),
-        comment_color = color::Fg(*COMMENT_COLOR),
-        tag_color = color::Fg(*TAG_COLOR),
-        snippet_color = color::Fg(*SNIPPET_COLOR),
+        "{comment} {tags} \n{snippet}",
+        comment = style(comment).with(*COMMENT_COLOR),
+        tags = style(format!("[{}]", tags)).with(*TAG_COLOR),
+        snippet = style(writer::fix_newlines(snippet)).with(*SNIPPET_COLOR),
     );
 }
 
@@ -57,9 +62,8 @@ pub fn preview_var(selection: &str, query: &str, variable: &str) {
     let delimiter = env::var(env_vars::PREVIEW_DELIMITER).ok();
     let map = env::var(env_vars::PREVIEW_MAP).ok();
 
-    let reset = color::Fg(color::Reset);
-    let active_color = color::Fg(*TAG_COLOR);
-    let inactive_color = color::Fg(*COMMENT_COLOR);
+    let active_color = *TAG_COLOR;
+    let inactive_color = *COMMENT_COLOR;
 
     let mut colored_snippet = String::from(snippet);
     let mut visited_vars: HashSet<&str> = HashSet::new();
@@ -67,12 +71,9 @@ pub fn preview_var(selection: &str, query: &str, variable: &str) {
     let mut variables = String::from("");
 
     println!(
-        "{comment_color}{comment} {tag_color}{tags}{reset}",
-        comment = comment,
-        tags = format!("[{}]", tags),
-        comment_color = color::Fg(*COMMENT_COLOR),
-        tag_color = color::Fg(*TAG_COLOR),
-        reset = reset,
+        "{comment} {tags}",
+        comment = style(comment).with(*COMMENT_COLOR),
+        tags = style(format!("[{}]", tags)).with(*TAG_COLOR),
     );
 
     let bracketed_current_variable = format!("<{}>", variable);
@@ -110,20 +111,16 @@ pub fn preview_var(selection: &str, query: &str, variable: &str) {
         };
 
         let replacement = format!(
-            "{color}{variable}{reset}",
-            color = variable_color,
-            variable = bracketed_variable_name,
-            reset = reset
+            "{variable}",
+            variable = style(bracketed_variable_name).with(variable_color),
         );
 
         colored_snippet = colored_snippet.replace(bracketed_variable_name, &replacement);
 
         variables = format!(
-            "{variables}\n{color}{variable}{reset} = {value}",
+            "{variables}\n{variable} = {value}",
             variables = variables,
-            color = variable_color,
-            variable = variable_name,
-            reset = reset,
+            variable = style(variable_name).with(variable_color),
             value = finder::process(value, column, delimiter.as_deref(), map.clone())
                 .expect("Unable to process value"),
         );
@@ -166,13 +163,10 @@ impl Writer {
 impl writer::Writer for Writer {
     fn write(&mut self, item: Item) -> String {
         format!(
-            "{tag_color}{tags_short}{delimiter}{comment_color}{comment_short}{delimiter}{snippet_color}{snippet_short}{delimiter}{tags}{delimiter}{comment}{delimiter}{snippet}{delimiter}{file_index}{delimiter}\n",
-            tags_short = limit_str(item.tags, self.tag_width),
-            comment_short = limit_str(item.comment, self.comment_width),
-            snippet_short = writer::fix_newlines(item.snippet),
-            comment_color = color::Fg(*COMMENT_COLOR),
-            tag_color = color::Fg(*TAG_COLOR),
-            snippet_color = color::Fg(*SNIPPET_COLOR),
+            "{tags_short}{delimiter}{comment_short}{delimiter}{snippet_short}{delimiter}{tags}{delimiter}{comment}{delimiter}{snippet}{delimiter}{file_index}{delimiter}\n",
+            tags_short = style(limit_str(item.tags, self.tag_width)).with(*TAG_COLOR),
+            comment_short = style(limit_str(item.comment, self.comment_width)).with(*COMMENT_COLOR),
+            snippet_short = style(writer::fix_newlines(item.snippet)).with(*SNIPPET_COLOR),
             tags = item.tags,
             comment = item.comment,
             delimiter = writer::DELIMITER,
