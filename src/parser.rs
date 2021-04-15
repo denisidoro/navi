5532b233929e5881d976e81faf8a040dfac54244
Denis Isidoro <denis.isidoro@uber.com>
2021-04-15 10:48:33 -0300

    Apr-15 10h48: parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -139,6 +139,7 @@ fn without_prefix(line: &str) -> String {
     }
 }
 
+#[allow(clippy::too_many_arguments)]
 pub fn read_lines(
     lines: impl Iterator<Item = Result<String, Error>>,
     id: &str,
