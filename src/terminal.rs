fe821a818a4217b56e68d289284ff8654d7da483
Denis Isidoro <denis.isidoro@uber.com>
2021-04-16 14:01:07 -0300

    Apr-16 14h01: terminal.rs

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,10 +1,11 @@
+use anyhow::Result;
 pub use crossterm::style;
 use crossterm::terminal;
 use std::str::FromStr;
 
 const FALLBACK_WIDTH: u16 = 80;
 
-fn width_with_shell_out() -> u16 {
+fn width_with_shell_out() -> Result<u16> {
     use std::process::Command;
     use std::process::Stdio;
 
@@ -14,37 +15,35 @@ fn width_with_shell_out() -> u16 {
             .arg("/dev/stderr")
             .arg("size")
             .stderr(Stdio::inherit())
-            .output()
-            .expect("Failed to execute stty")
+            .output()?
     } else {
         Command::new("stty")
             .arg("size")
             .arg("-F")
             .arg("/dev/stderr")
             .stderr(Stdio::inherit())
-            .output()
-            .expect("Failed to execute stty")
+            .output()?
     };
 
-    match output.status.code() {
-        Some(0) => {
-            let stdout = String::from_utf8(output.stdout).expect("Invalid utf8 output from stty");
-            let mut data = stdout.split_whitespace();
-            data.next();
-            data.next()
-                .expect("Not enough data")
-                .parse::<u16>()
-                .expect("Invalid base-10 number")
-        }
-        _ => FALLBACK_WIDTH,
+    if let Some(0) = output.status.code() {
+        let stdout = String::from_utf8(output.stdout).expect("Invalid utf8 output from stty");
+        let mut data = stdout.split_whitespace();
+        data.next();
+        return data
+            .next()
+            .expect("Not enough data")
+            .parse::<u16>()
+            .map_err(|_| anyhow!("Invalid width"));
     }
+
+    Err(anyhow!("Invalid status code"))
 }
 
 pub fn width() -> u16 {
     if let Ok((w, _)) = terminal::size() {
         w
     } else {
-        width_with_shell_out()
+        width_with_shell_out().unwrap_or(FALLBACK_WIDTH)
     }
 }
 
