ca3658be0d2e98219228f1e6442801aff5dcbb89
Denis Isidoro <denis.isidoro@uber.com>
2021-04-19 07:04:26 -0300

    No -c in cmd.exe

--- src/actor.rs
+++ src/actor.rs
@@ -26,7 +26,7 @@ fn prompt_finder(
     env_var::remove(env_var::PREVIEW_DELIMITER);
     env_var::remove(env_var::PREVIEW_MAP);
 
-    let mut extra_preview = None;
+    let mut extra_preview: Option<String> = None;
 
     let (suggestions, initial_opts) = if let Some(s) = suggestion {
         let (suggestion_command, suggestion_opts) = s;
@@ -42,7 +42,7 @@ fn prompt_finder(
                 env_var::set(env_var::PREVIEW_MAP, m);
             }
             if let Some(p) = &sopts.preview {
-                extra_preview = Some(format!(";echo;{}", p));
+                extra_preview = Some(p.into());
             }
         }
 
@@ -78,7 +78,7 @@ fn prompt_finder(
     let exe = fs::exe_string()?;
     let extra = extra_preview.clone().unwrap_or_default();
 
-    let preview = if cfg!(target_os = "macos") {
+    let preview = if cfg!(target_os = "windows") {
         format!(
             r#"(@echo.{{+}}{eof}{{q}}{eof}{name}){eof}{extra} | {exe} preview-var-stdin"#,
             exe = exe,
