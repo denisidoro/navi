c08638918d0afaebba125815f0445f5b28239896
Denis Isidoro <denis.isidoro@uber.com>
2021-08-07 15:43:03 -0300

    Aug-07 15h43: navi.plugin.fish

--- shell/navi.plugin.fish
+++ shell/navi.plugin.fish
@@ -1,22 +1,22 @@
 #!/usr/bin/env fish
 
 function __call_navi
-    navi --print
+navi --print
 end 
 
 function navi-widget -d "Show cheat sheets"
-  begin
-    set ttysettings (stty -g)
-    stty sane
-    __call_navi | perl -pe 'chomp if eof' | read -lz result
-    and commandline -- $result
+	begin
+set ttysettings (stty -g)
+	stty sane
+	__call_navi | perl -pe 'chomp if eof' | read -lz result
+	and commandline -- $result
 
-    stty $ttysettings
-  end
-  commandline -f repaint
-end
+	stty $ttysettings
+	end
+	commandline -f repaint
+	end
 
-bind \cg navi-widget
-if bind -M insert > /dev/null 2>&1
-  bind -M insert \cg navi-widget
-end
+	bind \cg navi-widget
+	if bind -M insert > /dev/null 2>&1
+	bind -M insert \cg navi-widget
+	end
