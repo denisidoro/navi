9f35e3ef6963331b740bb0bf5fd7cd1cabf005de
Kid <kidonng@gmail.com>
2022-01-16 19:17:28 +0800

    Quotes

--- shell/navi.plugin.fish
+++ shell/navi.plugin.fish
@@ -1,18 +1,18 @@
 function _navi_smart_replace
     set -l current_process (commandline -p | string trim)
 
-    if test -z $current_process
+    if test -z "$current_process"
         commandline -i (navi --print)
     else
-        set -l best_match (navi --print --best-match --query $current_process)
+        set -l best_match (navi --print --best-match --query "$current_process")
 
-        if not test $best_match >/dev/null
+        if not test "$best_match" >/dev/null
             return
         end
 
-        if test $best_match = ""
+        if test -z "$best_match"
             commandline -p (navi --print --query $current_process)
-        else if test $current_process != $best_match
+        else if test "$current_process" != "$best_match"
             commandline -p $best_match
         else
             commandline -p (navi --print --query $current_process)
