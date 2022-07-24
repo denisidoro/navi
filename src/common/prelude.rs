0044d8e868ddbc4dbb30d2ff4d6e04550c6f131f
Denis Isidoro <denis.isidoro@uber.com>
2022-07-24 17:14:46 -0300

    Jul-24 17h14: prelude.rs, prelude.rs

--- src/common/prelude.rs
+++ src/common/prelude.rs
@@ -1,6 +1,7 @@
-pub use super::component::Component;
-pub use super::deps::HasDeps;
-pub use super::fs::ToStringExt;
+// pub use super::component::Component;
+// pub use super::deps::HasDeps;
+// pub use super::fs::ToStringExt;
+pub use crate::common::fs::ToStringExt;
 pub use anyhow::{anyhow, Context, Error, Result};
 pub use serde::de::Deserializer;
 pub use serde::ser::Serializer;
@@ -8,19 +9,15 @@ pub use serde::{Deserialize, Serialize};
 pub use std::any::{Any, TypeId};
 pub use std::collections::{HashMap, HashSet};
 pub use std::convert::{TryFrom, TryInto};
+pub use std::fmt::Debug;
 pub use std::fs::File;
-pub use std::io::BufReader;
+pub use std::io::{BufRead, BufReader};
 pub use std::path::{Path, PathBuf};
+pub use std::process::Stdio;
+pub use std::str::FromStr;
 pub use std::sync::{Arc, Mutex, RwLock};
 // pub use tracing::{self, debug, error, event, info, instrument, span, trace, warn};
-/*
-pub extern crate anyhow;
-pub extern crate serde;
-pub extern crate tracing_subscriber;
 
-#[cfg(feature = "yaml")]
-pub extern crate serde_yaml;
-
-#[cfg(feature = "json")]
-pub extern crate serde_json;
- */
+// pub extern crate anyhow;
+// pub extern crate serde;
+// pub extern crate tracing_subscriber;
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,32 +1,8 @@
-// pub use super::component::Component;
-// pub use super::deps::HasDeps;
-// pub use super::fs::ToStringExt;
-pub use anyhow::{anyhow, Context, Error, Result};
-pub use serde::de::Deserializer;
-pub use serde::ser::Serializer;
-pub use serde::{Deserialize, Serialize};
-pub use std::any::{Any, TypeId};
-pub use std::collections::{HashMap, HashSet};
-pub use std::convert::{TryFrom, TryInto};
-pub use std::fmt::Debug;
-pub use std::fs::File;
-pub use std::io::{BufRead, BufReader};
-pub use std::path::{Path, PathBuf};
-pub use std::sync::{Arc, Mutex, RwLock};
-// pub use tracing::{self, debug, error, event, info, instrument, span, trace, warn};
-
-// pub extern crate anyhow;
-// pub extern crate serde;
-// pub extern crate tracing_subscriber;
-
+pub use crate::common::prelude::*;
 pub use crate::config::CONFIG; // TODO
 pub use regex::Regex;
-pub use std::str::FromStr;
 
-pub use std::process::Stdio;
-
-pub use crate::common::fs::ToStringExt;
-pub use crate::fs::pathbuf_to_string; // TODO
+// pub use crate::fs::pathbuf_to_string; // TODO
 
 pub trait Runnable {
     fn run(&self) -> Result<()>;
