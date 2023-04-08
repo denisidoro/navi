9349a119412a47adc589df7bebf7b196ba1eb21f
Denis Isidoro <denis.isidoro@uber.com>
2023-04-08 19:33:20 -0300

    Apr-08 19h33: Cargo.lock, prelude.rs, Cargo.toml

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,18 @@
-pub use crate::common::prelude::*;
 pub use crate::config::CONFIG; // TODO
+pub use dns_common::prelude::*;
 pub use regex::Regex;
+
+pub use crate::common::fs::ToStringExt;
+pub use std::any::{Any, TypeId};
+pub use std::collections::{HashMap, HashSet};
+pub use std::convert::{TryFrom, TryInto};
+pub use std::fmt::Debug;
+pub use std::fs::File;
+pub use std::io::{BufRead, BufReader};
+pub use std::path::{Path, PathBuf};
+pub use std::process::Stdio;
+pub use std::str::FromStr;
+pub use std::sync::{Arc, Mutex, RwLock};
 
 // pub use crate::common::fs::pathbuf_to_string; // TODO
 
