51f7419c26b972acf8c91455b491cead9ecf7f6e
Denis Isidoro <denis.isidoro@uber.com>
2023-04-08 19:38:18 -0300

    Apr-08 19h38: prelude.rs, mod.rs, fs.rs

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,13 +1,7 @@
-// pub mod component;
-// pub mod deps;
-// pub mod deser;
 pub mod clipboard;
 pub mod fs;
 pub mod git;
 pub mod hash;
-pub mod prelude;
 pub mod shell;
 pub mod terminal;
 pub mod url;
-// pub mod system;
-// pub mod tracing;
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,7 @@
 pub use crate::config::CONFIG; // TODO
 pub use dns_common::prelude::*;
 pub use regex::Regex;
+pub use std::io::{BufRead, BufReader};
 pub use std::process::Stdio;
 pub use std::str::FromStr;
 
