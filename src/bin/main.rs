2755d51b38a9badf28fa2b041b533aa7f0e8998d
zjp <jiping_zhou@foxmail.com>
2023-05-12 23:32:00 +0800

    mv navi.log under config dir; debug folders on Windows
    2023-05-12T15:51:54.280707Z DEBUG navi::filesystem: read cheat files in `"C"`: []
    2023-05-12T15:51:54.281083Z DEBUG navi::filesystem: read cheat files in `"\\Users\\Administrator\\AppData\\Roaming\\navi\\cheats"`: []

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,6 @@
 extern crate navi;
 
-use dns_common::prelude::{debug, error, tracing, tracing_subscriber};
+use dns_common::prelude::*;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -33,14 +33,18 @@ fn main() -> Result<(), anyhow::Error> {
 }
 
 fn init_logger() -> anyhow::Result<()> {
+    const FILE_NAME: &str = "navi.log";
+    let mut file = navi::default_config_pathbuf()?;
+    file.set_file_name(FILE_NAME);
+
     tracing::subscriber::set_global_default(
         tracing_subscriber::fmt()
             .with_ansi(false)
-            // TODO: config_path/navi.log
-            .with_writer(std::fs::File::create("navi.log")?)
+            .with_writer(std::fs::File::create(file)?)
             .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
             .finish(),
     )?;
     debug!("tracing initialized");
+
     Ok(())
 }
