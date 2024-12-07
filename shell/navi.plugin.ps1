a89116c8f2fb3fa139e278d3543b373fca2860f9
alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
2024-12-07 14:00:36 +0100

    Moved Powershell navi plugin to its own subfolder
    Moved the plugin as a module in order to have the Invoke-NaviWidget function defined in every session
    
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>

--- shell/navi.plugin.ps1
+++ shell/navi.plugin.ps1
@@ -1,26 +1,60 @@
+### This script installs the Powershell module under the user's Powershell modules path
+### For modifications of the Powershell Navi plugin, see /shell/navi.plugin/navi.plugin.psm1.
 
-### Initial code from @lurebat (https://github.com/lurebat/)
-function Invoke-NaviWidget {
-    $ast = $tokens = $errors = $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref] $ast, [ref] $tokens, [ref] $errors, [ref] $cursor)
+param (    
+    ### Controls if we update the current module (otherwise we don't rewrite the already existing module, if present)
+    [Switch]$Update,
+    ### Should the verbosity be enabled?
+    [Switch]$Verbose
+)
 
-    $line = $ast.ToString().Trim()
-    $output = $null
 
-    if ([String]::IsNullOrEmpty($line)) {
-        $output = navi --print
+function InstallNaviWidgetModule(){
+    [String]$PwshModulePath = $env:PSModulePath.Split(";")[0];
+    [String]$NAVI_PLUGIN = "navi.plugin";
+
+    Write-Debug "Are we updating the PowerShell module: $Update"
+    Write-Debug "Current Root Powershell Module path is: $PwshModulePath"
+
+    ### If we're updating the module, Copy the newly updated contents to the currently installed module
+    ### then quit with a successful exit code.
+    ### We're not supposed to update the shortcut binding.
+    if ($Update){
+        Write-Debug "Updating Navi-Widget PowerShell module"
+        Copy-Item -Path .\navi.plugin\ -Destination $PwshModulePath -Force -Recurse
+        exit 0
     }
-    else {
-        $best_match = (navi --print --best-match --query $line | Out-String).Trim()
-        if ([String]::IsNullOrEmpty($best_match)) {
-            $output = (navi --print --query "$line" | Out-String).Trim()
-        } else {
-            $output = $best_match
-        }
+
+    ### If we're not updating, check if we don't have the module already installed
+    if (-Not (Test-Path -Path $PwshModulePath\$NAVI_PLUGIN)) {
+        Write-Debug "Copying Item to the path"
+        Copy-Item -Path .\navi.plugin\ -Destination $PwshModulePath -Recurse
+    } else {
+        Write-Error "Navi-Widget is already installed for PowerShell!"
+        exit 1
     }
 
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($output)
+    Write-Debug "Registering the navi shortcut inside the current shell session"
+    Set-PSReadlineKeyHandler -BriefDescription "A keybinding to open Navi Widget" -Chord Ctrl+g -ScriptBlock { Invoke-NaviWidget }
+
+    Write-Debug "Appending the navi shortcut inside the current user's profile"
+    ### Adding a new line
+    Write-Output "Import-Module navi.plugin" >> $PROFILE
+    Write-Output "" >> $PROFILE
+    Write-Output 'Set-PSReadlineKeyHandler -BriefDescription "A keybinding to open Navi Widget" -Chord Ctrl+g -ScriptBlock { Invoke-NaviWidget }' >> $PROFILE
+
+
+    Write-Output "Navi plugin has been installed!"
+    exit 0 ### Succesful installation
 }
 
-Set-PSReadlineKeyHandler -Key Ctrl+g -ScriptBlock { Invoke-NaviWidget }
+if ($Verbose) {
+    ### Enabling verbose/debug output at the start of the script
+    $DebugPreference = 'Continue'
+    InstallNaviWidgetModule -Update $Update
+    ### Disabling verbose/debug output at the end of the script
+    ### in order to not modify the current user's shell environment
+    $DebugPreference = 'SilentlyContinue'
+} else {
+    InstallNaviWidgetModule -Update $Update
+}
