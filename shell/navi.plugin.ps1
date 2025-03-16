fbc94b0a3e1c0f433cdc9518883d123c148f78a2
alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
2025-03-16 07:44:49 +0100

    Update powershell plugin definition and exports
    Co-authored-by: KITAGAWA Yasutaka <kit494way@gmail.com>
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>

--- shell/navi.plugin.ps1
+++ shell/navi.plugin.ps1
@@ -1,6 +1,24 @@
 
 
 $null = New-Module {
+
+    function Invoke-Navi {
+        $startArgs = @{
+            FileName = "navi";
+            Arguments = $args;
+            RedirectStandardOutput = $true;
+            WorkingDirectory = $PWD;
+        }
+        $p = [System.Diagnostics.Process]@{StartInfo = $startArgs}
+
+        [void]$p.Start()
+        $result = $p.StandardOutput.ReadToEnd()
+        $p.WaitForExit()
+
+        $result
+    }
+
+
     ### Initial code from @lurebat (https://github.com/lurebat/)
     ### See #570 (https://github.com/denisidoro/navi/issues/570) for its original contribution
     function Invoke-NaviWidget {
@@ -11,19 +29,19 @@ $null = New-Module {
         $output = $null
 
         if ([String]::IsNullOrEmpty($line)) {
-            $output = navi --print
+            $output = (Invoke-Navi "--print" | Out-String).Trim()
         }
         else {
-            $best_match = (navi --print --best-match --query $line | Out-String).Trim()
+            $best_match = (Invoke-Navi "--print --best-match --query `"$line`"" | Out-String).Trim()
             if ([String]::IsNullOrEmpty($best_match)) {
-                $output = (navi --print --query "$line" | Out-String).Trim()
+                $output = (Invoke-Navi "--print --query `"$line`"" | Out-String).Trim()
             }
             else {
                 $output = $best_match
             }
         }
 
-        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
 
         ### Handling the case when the user escapes without selecting any entry
         if (-Not([String]::IsNullOrEmpty($output))) {
@@ -32,4 +50,5 @@ $null = New-Module {
     }
 
     Set-PSReadlineKeyHandler -BriefDescription "A keybinding to open Navi Widget" -Chord Ctrl+g -ScriptBlock { Invoke-NaviWidget }
+    Export-ModuleMember -Function @()
 }
