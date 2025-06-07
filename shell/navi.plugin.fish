a7efad2740096b75dbf81bbf12231127368ea572
Lam Chau <lamchau@gmail.com>
2025-06-07 14:36:02 -0700

    docs: add references to fish

--- shell/navi.plugin.fish
+++ shell/navi.plugin.fish
@@ -8,6 +8,13 @@ function _navi_smart_replace
     end
 
     set --local force_repaint false
+    # https://github.com/fish-shell/fish-shell/blob/d663f553dffba460d6d0bcdf93df21bda9ec6f3f/doc_src/interactive.rst?plain=1#L440
+    #  > Bindings that change the mode are supposed to call the repaint-mode bind function
+    #
+    # Related issues
+    #  - https://github.com/fish-shell/fish-shell/issues/5033
+    #  - https://github.com/fish-shell/fish-shell/issues/5860
+    #  - https://github.com/fish-shell/fish-shell/blob/d663f553dffba460d6d0bcdf93df21bda9ec6f3f/src/screen.rs#L531
     if test $version_parts[1] -ge 4
         set force_repaint true
     end
